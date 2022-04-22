7e43e9e4017785d16eecf5d153ebc5e8eaa4c211
Kyo Lee <kyo.se.lee@gmail.com>
2022-04-22 09:12:19 -0700

    Created a basic database structure. This database structure has a User class (id, username, email, password_hash), and an Item class (id, name, price, image_url). The database has been tested in ipython3, and works as expected.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 myapp_obj = Flask(__name__)
 
-from app import routes
+db = SQLAlchemy(myapp_obj)
+
+from app import routes, models
