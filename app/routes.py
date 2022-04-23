c1c4a633aaf57fb027698dcad00aceeccf017a73
Kyo Lee <kyo.se.lee@gmail.com>
2022-04-22 20:03:44 -0700

    Built up a basic Navigation Bar in Bootstrap. Expand toggle button is not yet working.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import myapp_obj
+from flask import flash, render_template
 
 @myapp_obj.route("/")
 @myapp_obj.route("/home")
 def home():
-	return "<html><p>Hello, World!</p></html>"
+	return render_template("home.html")
