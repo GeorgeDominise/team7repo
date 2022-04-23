bfe00060743fce41ef75377f78cb6e07f500106c
Kyo Lee <kyo.se.lee@gmail.com>
2022-04-22 20:42:03 -0700

    Updated the database a little bit, adding a Sign In/Register button, and a User profile button in the headbar. Please note that none of these functions lead anywhere, yet.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from app import myapp_obj
 from flask import flash, render_template
 
+login_status = False # Temporary variable to test redirects based on whether user is logged in or not
+username = "Team7 Shared Account"
+
 @myapp_obj.route("/")
 @myapp_obj.route("/home")
 def home():
-	return render_template("home.html")
+	return render_template("home.html", login_status=login_status, username=username)
