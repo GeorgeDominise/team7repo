87b7da77af06e731cfb03a28aea00015e570b2bc
Kyo Lee <kyo.se.lee@gmail.com>
2022-04-24 11:47:17 -0700

    Finished the basic structure of the VANILUXE website. Created blank webpages for 'about', 'faqs', 'featured', 'purchase', 'register'. and 'signin'.

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,28 @@ username = "Team7 Shared Account"
 @myapp_obj.route("/")
 @myapp_obj.route("/home")
 def home():
-	return render_template("home.html", login_status=login_status, username=username)
+	return render_template("home.html", login_status=login_status)
+
+@myapp_obj.route("/featured")
+def featured():
+	return render_template("featured.html", login_status=login_status)
+
+@myapp_obj.route("/purchase")
+def purchase():
+	return render_template("purchase.html", login_status=login_status)
+
+@myapp_obj.route("/signin")
+def signin():
+	return render_template("signin.html", login_status=login_status)
+
+@myapp_obj.route("/register")
+def register():
+	return render_template("register.html", login_status=login_status)
+
+@myapp_obj.route("/faqs")
+def faqs():
+	return render_template("faqs.html", login_status=login_status)
+
+@myapp_obj.route("/about")
+def about():
+	return render_template("about.html", login_status=login_status)
