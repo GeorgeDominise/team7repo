f04d5c27424c6c20e7c1bca11c4ae8fc0141a6b5
jazSingh5 <44950413+jazSingh5@users.noreply.github.com>
2022-05-02 07:17:21 -0700

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,12 @@ class SellForm(FlaskForm):
 	description = StringField("Description")
 	image_url = StringField("Image URL")
 	submit = SubmitField("Submit")
+
+	
+class RegisterForm(FlaskForm):
+#Allows users to make a new account
+    username = StringField("Enter your username:", validators=[DataRequired()])
+    email = StringField("Enter an email:", validators=[DataRequired()])
+    password = PasswordField("Enter a password:", validators=[DataRequired()])
+    password_ver = PasswordField("Please verify your password:", validators=[DataRequired()])
+    submit = SubmitField("Submit form")
