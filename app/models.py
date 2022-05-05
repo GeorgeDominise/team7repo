3c018a13b03313db308caaac7b28f440204b5fa0
Rohan Maheshwari <jyotihome@Rohans-Macbook.local>
2022-05-04 22:50:22 -0700

    Updated models.py on rohansbranch

--- app/models.py
+++ app/models.py
@@ -1,28 +1,16 @@
-from app import db, login
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from app import db
 
-
-class User(UserMixin, db.Model):
+class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(128), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
+	registered = db.Column(db.Boolean)
 
 	items = db.relationship("Item", backref="author", lazy="dynamic")
 
-	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
-		
-	def check_password(self, password):
-		return check_password_hash(self.password_hash, password)
-	
-	@login.user_loader
-	def load_user(id):
-		return User.query.get(int(id))
-
 	def __repr__(self):
-		return "User: {}".format(self.username)
+		return "<User {}>".format(self.username)
 
 class Item(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
