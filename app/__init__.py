153236c5abec841f51e82e2cbbd176213f743b3e
Kyo Lee <kyo.se.lee@gmail.com>
2022-05-13 16:04:05 -0700

    Created the 'About Us' page. Also created a footer. ALSO created a static/ folder structure to fit images, javascript, and css. ALSO (my goodness where does it end) created some CSS for base.html, thus affecting everything. Please inform me if this affected your page negatively - I will show you how to fix it.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_msearch import Search
 from flask_login import LoginManager
+from flask_migrate import Migrate
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,6 +16,7 @@ myapp_obj.config.from_mapping(
 )
 
 db = SQLAlchemy(myapp_obj)
+migrate = Migrate(myapp_obj, db)
 
 search = Search(db=db)
 search.init_app(myapp_obj)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from flask_login import UserMixin, LoginManager
+from datetime import datetime
 
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -22,8 +23,11 @@ class Item(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(128), index=True)
 	price = db.Column(db.Integer, index=True)
+#	stock = db.Column(db.Integer)
 	description = db.Column(db.String(2048))
 	image_url = db.Column(db.String(256))
+
+#	timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 	user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
